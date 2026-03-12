d90ad54a674ec9bcdf7235ca043769db1a29eede
David <davidsarria911@hotmail.com>
2026-03-11 19:28:14 -0500

    feat(Profiles): Implementing Profile model and fixing User's last login date AB#122

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
 from models.user import User
+from models.profile import Profile, EnglishLevel, EmploymentType
 from models.token_blacklist import TokenBlacklist
 from models.refresh_token import RefreshToken  
-from models.token_blacklist import TokenBlacklist  
